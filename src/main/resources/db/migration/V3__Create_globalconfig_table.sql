73f5cb0a833a3ac92a56fefbd7edbc1c406ff491
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-10-21 00:40:03 +0200

    add condense age persistence

--- src/main/resources/db/migration/V3__Create_globalconfig_table.sql
+++ src/main/resources/db/migration/V3__Create_globalconfig_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE global_config
 (
     ID INT UNIQUE,
-    schedule_rate INT NOT NULL,
+    schedule_rate INT NOT NULL DEFAULT 3600000,
     PRIMARY KEY(ID),
 	CHECK (ID = 1)
 )
 
-INSERT INTO global_config
-VALUES (1, 3600000)
+INSERT INTO global_config (ID)
+VALUES (1)
