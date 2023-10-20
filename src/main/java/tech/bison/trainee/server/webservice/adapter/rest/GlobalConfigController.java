3e0f52ab0820209335fe99750cba1b3889a559b7
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-10-21 00:50:45 +0200

    only respond with dtos

--- src/main/java/tech/bison/trainee/server/webservice/adapter/rest/GlobalConfigController.java
+++ src/main/java/tech/bison/trainee/server/webservice/adapter/rest/GlobalConfigController.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import tech.bison.trainee.server.business.domain.global_config.GlobalConfig;
 import tech.bison.trainee.server.business.service.CondenseService;
 import tech.bison.trainee.server.business.service.GlobalConfigService;
 import tech.bison.trainee.server.webservice.adapter.model.global_config.GlobalConfigDto;
@@ -31,9 +31,9 @@ public class GlobalConfigController {
   }
 
   @PutMapping
-  ResponseEntity<GlobalConfig> update(GlobalConfigDto globalConfigDto) {
-    ResponseEntity<GlobalConfig> response = ResponseEntity
-        .ok(service.update(webMapperService.fromDto(globalConfigDto)));
+  ResponseEntity<GlobalConfigDto> update(@RequestBody GlobalConfigDto globalConfigDto) {
+    ResponseEntity<GlobalConfigDto> response = ResponseEntity
+        .ok(webMapperService.toDto(service.update(webMapperService.fromDto(globalConfigDto))));
     condenseService.updateScheduler();
     return response;
   }
